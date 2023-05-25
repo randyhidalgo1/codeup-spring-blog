acdd9a1a6c8ee5d180e858ce016a388bb2140054
Randy Hidalgo <randy.x.hidalgo@gmail.com>
2023-05-25 01:00:53 -0500

    Views-Exercises

--- src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
@@ -1,32 +1,52 @@
 package com.codeup.codeupspringblog.controllers;
 
+import com.codeup.codeupspringblog.models.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
 @Controller
 public class PostController {
+    @GetMapping("/posts")
+    public String allPosts(Model model) {
+        List<Post> posts = new ArrayList<>();
+        Post post1 = new Post();
+        post1.setTitle("First Post");
+        post1.setBody("This is the first post.");
+        posts.add(post1);
 
-    @GetMapping ("/posts")
-    @ResponseBody
-    public String allPosts() {
-        return "posts index page";
+        Post post2 = new Post();
+        post2.setTitle("Second Post");
+        post2.setBody("This is the second post.");
+        posts.add(post2);
+
+        model.addAttribute("posts", posts);
+        return "posts/index";
     }
-    @GetMapping ("/posts/{id}")
-    @ResponseBody
-    public String individualPost() {
-        return "View an individual post";
+
+    @GetMapping("/posts/{id}")
+    public String individualPost(@PathVariable int id, Model model) {
+        Post post = new Post();
+        post.setTitle("Post " + id);
+        post.setBody("This is the post with id " + id + ".");
+        model.addAttribute("post", post);
+        return "posts/show";
     }
-    @GetMapping ("/posts/create")
-    @ResponseBody
+
+    @GetMapping("/posts/create")
     public String createPosts() {
-        return "View form for creating a post";
-    }
-    @PostMapping("/posts/create")
-    @ResponseBody
-    public String submitPosts() {
-        return "Creating a new post";
+        return "posts/create";
     }
 
+    @PostMapping("/posts/create")
+    public String submitPosts() {
+        // Logic for creating a new post
+        return "redirect:/posts";
+    }
 }
+
+
---
+++ src/main/java/com/codeup/codeupspringblog/models/Post.java
@@ -0,0 +1,23 @@
+package com.codeup.codeupspringblog.models;
+
+public class Post {
+    private String title;
+    private String body;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
+
